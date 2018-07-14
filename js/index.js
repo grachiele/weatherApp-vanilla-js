fad2394396b42bfce329195077c12c67733b2c5b
Gianpaul Rachiele <gianpaul817@gmail.com>
2018-07-14 18:56:21 -0400

    refactoring

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 function onSubmit(e) {
   e.preventDefault();
 
-  let zipcode = event.target.elements[0].value;
+  let zipcode = e.target.elements[0].value;
   const APIKEY = "1b3292d8476ca73faee1a365e843fbb1"
 ;
 
@@ -11,29 +11,43 @@ function onSubmit(e) {
   if (zipcode === "") {
     alert("You need to enter a zipcode!")
   } else {
-    let fiveDayWeather = fetch(weatherUrl)
+    fetch(weatherUrl)
       .then((res) => {
         return res.json()
       })
       .then((response) => {
-        let forecast = response.list;
-        let city = response.city
-
-        let fiveDay = []
-
-        for (index in forecast) {
-          let weather = forecast[index];
-          if (index === 0) {
-            fiveDay.push(weather);
-          }
-          if (index % 8 === 0){
-            fiveDay.push(weather);
-          }
-        }
-
-        return fiveDay
+        let fiveDay = getFiveDay(response);
+        showWeather(fiveDay);
       });
+    }
+}
+
+function getFiveDay(obj) {
+  let forecast = obj.list;
+  let city = obj.city
+
+  let fiveDay = []
+
+  for (index in forecast) {
+    let weather = forecast[index];
+    if (index === 0) {
+      fiveDay.push(weather);
+    }
+    if (index % 8 === 0){
+      fiveDay.push(weather);
+    }
   }
 
-  console.log(fiveDayWeather);
+  let newObj = {
+    city,
+    fiveDay
+  }
+
+  return newObj
+}
+
+function showWeather(obj) {
+  console.log(obj);
+  let weatherArea = document.querySelector("#weather-area");
+  let city = `<h1 id="current-city">${obj.city}</h1>`
 }
